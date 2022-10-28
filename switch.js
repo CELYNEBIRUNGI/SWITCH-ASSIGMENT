e16a6f34a4e143f41b940fb78985b4c4e5ba124b
CELYNEBIRUNGI <birungilyn@gmail.com>
2022-10-28 13:50:03 +0300

    switch commit

---
+++ switch.js
@@ -0,0 +1,26 @@
+function checkHere(elevator){
+    switch (elevator)
+       {
+       case "level 1":
+        console.log("This is the level 1")
+        break
+       case "level 2":
+        console.log("This is the level 2")
+        break
+       case "level 3":
+        console.log("This is the  level 3")
+        break
+            alert('stop');
+            break;
+      case "ss":
+           alert("elevator up");
+           break;
+     default:
+           alert('elevatordown');
+           break;
+    
+       }
+
+
+
+    
